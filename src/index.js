7408a811c8efc31a9b4b2d90aa77b2f9070db5e4
Marcos Vinicius <marcos.oliveira.e@midway.com.br>
2021-09-27 20:56:20 -0300

    configurando theme no app

--- src/index.js
+++ src/index.js
@@ -1,14 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import { BrowserRouter } from 'react-router-dom';
+import { ThemeProvider } from 'styled-components';
 import App from './App';
+import theme from './styles/themes';
 import GlobalStyle from './styles/global';
 
 ReactDOM.render(
   <BrowserRouter>
-    <GlobalStyle>
-      <App />
-    </GlobalStyle>
+    <ThemeProvider theme={ theme }>
+      <GlobalStyle>
+        <App />
+      </GlobalStyle>
+    </ThemeProvider>
   </BrowserRouter>,
   document.getElementById('root'),
 );
---
+++ src/styles/themes/index.js
@@ -0,0 +1,11 @@
+export default {
+  colors: {
+    primary: '',
+    secondary: '',
+    textPrimary: '',
+    textSecondary: '',
+    textButton: '#333',
+    borderBox: '#00000029',
+    backgroundButton: '',
+  },
+};
